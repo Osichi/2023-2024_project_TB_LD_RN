98022019eedf77f918976af56352540ecaa7834c
Rácz Norbert <racz.norbert@labnet.tk>
2023-10-25 10:52:43 +0200

    readme.md

--- frontend/mainPage.php
+++ frontend/mainPage.php
@@ -6,23 +6,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/bootstrap.min.css" rel="stylesheet">
-    <script src="./js/bootstrap.bundle.min.js"></script>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <script src="../js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="mainPageStyle.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <h1>mainPage</h1>
     </div>
-
-    <ul>
+    <nav>
         <?php if (isset($_SESSION["user"])) {
-        echo"<li style='color:red;'>".$_SESSION['user']."</li>";
-        echo"<li><a href='../backend/logout.php'>Kilépés</a></li>";}
+            echo "Üdvözöllek ".$_SESSION['user']."!";
+            echo "<a href='profilePage.php'>Profilom</a>";
+            echo"<a href='../backend/logout.php'>Kilépés</a>";
+    }
     else {
-        echo"<li><a href='registration.php'>Regisztráció</a></li>";
-        echo"<li><a href='login.php'>Belépés</a></li>";}?>
-    </ul>
-
+        echo"<a class='link-info' href='registration.php'>Regisztráció</a>";
+        echo"<a class='link-success' href='login.php'>Belépés</a>";}?>
+    
+    </nav>
 </body>
 </html>
