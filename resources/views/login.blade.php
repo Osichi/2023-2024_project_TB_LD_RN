8b8a0d70f9b31baf879b3ad16fef34ae68958d78
TrajerBalazs <trajerbazsi@gmail.com>
2024-02-20 10:37:17 +0100

    valami32312

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,36 +8,40 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="{{url('css/loginreg.css')}}">
-    
+
     <title>Bejelentkezés</title>
 </head>
 
 <body>
-    
 
-<div class="main">
-            <input type="checkbox" id="chk" aria-hidden="true">
 
-            <div class="signup">
-                <form>
-                    <label for="chk" aria-hidden="true">Sign up</label>
-                    <input type="text" name="txt" placeholder="Username" required="">
-                    <input type="email" name="email" placeholder="Email" required="">
-                    <input type="password" name="pswd" placeholder="Password" required="">
-                    <button>Sign up</button>
-                </form>
-            </div>
+    <div class="main">
+        <input type="checkbox" id="chk" aria-hidden="true">
 
-            <div class="login">
-                <form>
-                    <label for="chk" aria-hidden="true">Login</label>
-                    <input type="email" name="email" placeholder="Email" required="">
-                    <input type="password" name="pswd" placeholder="Password" required="">
-                    <button>Login</button>
-                </form>
-            </div>
+        <div class="signup">
+            <form>
+                <label for="chk" aria-hidden="true">Regisztráció</label>
+                <small>Hozzon létre egy új fiókot!</small>
+                <input type="text" name="txt" placeholder="Cégnév" required="">
+                <input type="email" name="email" placeholder="Cégjegyzékszám" required="">
+                <input type="password" name="pswd" placeholder="Jelszó" required="">
+                <button>Regisztráció</button>
+                <a href="/" class="link">Vissza a főoldalra!</a>
+            </form>
         </div>
 
+        <div class="login">
+            <form>
+                <label for="chk" aria-hidden="true">Bejelentkezés</label>
+                <small>Lépjen be meglévő fiókjába!</small>
+                <input type="email" name="email" placeholder="Cégjegyzékszám" required="">
+                <input type="password" name="pswd" placeholder="Jelszó" required="">
+                <button>Belépés</button>
+                <a href="/">Vissza a főoldalra!</a>
+            </form>
+        </div>
+        
+    </div>
 </body>
 
 </html>
